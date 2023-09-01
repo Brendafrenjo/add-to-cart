19f5c12859c4e461ecd252e2ad0e647bbd9cc6af
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-09-01 23:28:52 +0300

    Readjusted the image and the search button

--- src/Images.js
+++ src/Images.js
@@ -1,10 +1,11 @@
 import React from "react";
+import './Images.css'
 import img1 from "./Images/add-to-cart.png";
 
 export default function Images() {
   return (
     <div className="Images">
-      <img src={img1} alt="Add To Cart" width="300"></img>
+      <img src={img1} alt="Add To Cart" width="200" className="icon"></img>
     </div>
   );
 }
