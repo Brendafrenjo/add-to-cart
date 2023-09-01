63d92c9fbab8171d0c3cbeeeae352698be6b323f
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-09-01 23:58:36 +0300

    Added event listiners

--- src/AddToCart.js
+++ src/AddToCart.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AddToCart.css";
 import Images from "./Images";
 
-export default function AddToCart(params) {
+export default function AddToCart() {
+  const [item, setItem] = useState(" ");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`Searching ${item}`);
+  }
+
+  function updateItem(event) {
+    setItem(event.target.value);
+  }
+
   return (
     <div className="AddToCart">
       <br />
       <Images />
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="search"
           placeholder="Type item..."
           autoFocus="on"
+          onChange={updateItem}
           className="form-control shadow search-item w-100 text-center"
         />
       </form>
