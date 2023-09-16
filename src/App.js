692036e6487c7c2b1c3af762472e7590ee1ed6a0
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-09-16 17:26:31 +0300

    Created redux store and added reducer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import "./App.css";
-import AddToCart from "./AddToCart"
+import AddToCart from "./AddToCart";
+import { useState, useEffect } from "react";
+import { createStore } from "redux";
+import { Provider, connect, useSelector, useDispatch } from "react-redux";
+
+const countReducer = function (state = 0, action) {
+  switch ((action, type)) {
+    case "ADD":
+      return state + 1;
+    case "SUBTRACT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(countReducer)
 
 function App() {
   return (
