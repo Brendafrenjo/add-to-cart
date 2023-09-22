e75d8e6941420ec8d74fc19a33383cfb2ac2f54a
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-09-22 23:56:56 +0300

    Updated ItemCreator

--- src/ItemCreator.js
+++ src/ItemCreator.js
@@ -1,40 +1,34 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { addItem } from "./action";
 
-class ItemCreator extends Component {
-  state = { text: "" };
+function ItemCreator(props) {
+  const [text, setText] = useState("");
 
-  handleChange = (event) => {
-    this.setState({ text: event.target.value });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.props.addItem(this.state.text);
-    this.setState({ text: "" });
-  };
-
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            onChange={this.handleChange}
-            value={state.text}
-          />
-          <input type="submit" value="Create" />
-        </form>
-      </div>
-    );
+  function handleChange(event) {
+    return setText(event.target.value);
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.addItem(text);
+    setText("");
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleChange} value={text} />
+        <input type="submit" value="Create" />
+      </form>
+    </div>
+  );
 }
 
-const mapDispatchToProps = (dispatch) => {
+function mapDispatchToProps(dispatch) {
   return {
     addItem: (text) => dispatch(addItem(text)),
   };
-};
+}
 
 export default connect(null, mapDispatchToProps)(ItemCreator);
