bdbaf651f42dda7e50bd0943923c7e70d3be8a6b
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-09-27 23:42:54 +0300

    Update ItemContainer.js

--- src/ItemContainer.js
+++ src/ItemContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import Item from "./Item";
 import { addItem, deleteItem } from "./action";
